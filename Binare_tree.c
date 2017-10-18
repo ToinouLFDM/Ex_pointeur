f612d2f61f63254fec2bec88fa167f137f103136
Unknown <antoine.barbier@epita.fr>
2017-10-18 02:46:24 +0200

    queue and base done

--- Binare_tree.c
+++ Binare_tree.c
@@ -3,24 +3,53 @@
 #include<stdlib.h>
 typedef struct BIN_TREE{
 int value;
-struct bin_tree *right;
-struct bin_tree  *left;
-
+struct BIN_TREE *right;
+struct BIN_TREE  *left;
 }bin_tree;
 
+typedef struct QUEUE{
+	struct QUEUE *next;
+	int value;
+}Queue;
+
 bin_tree *initree(int val);
-void setvalue(bin_tree *tree, int elem);
-void printleft(bin_tree *tree);
+void printleft_infix(bin_tree *tree);
+void add_left(int val,bin_tree *tree);
+void add_right(int val,bin_tree *tree);
+void set_value(int val,bin_tree *tree);
+
+
+//queue
+Queue *init_queue();
+int is_empty(Queue *queue);
+void enqueue(Queue *queue,int val);
+int dequeue(Queue *queue);
+int len_queue(Queue *queue);
 
 int main(int argc, char *argv[])
 {	
-	bin_tree tree=*initree(1);
-	//tree->left=bin_tree;
-	//tree->right=bin_tree;
-	printleft(&tree);
+	bin_tree *tree=initree(1);
+	add_left(2,tree);
+	add_left(3,tree->left);
+	add_right(4,tree->left);
+	add_right(5,tree);
+	printleft_infix(tree);
+	set_value(15,tree);
+	printleft_infix(tree);
+	
+	Queue *queue=init_queue();
+	enqueue(queue,10);
+	enqueue(queue,12);
+	enqueue(queue,14);
+	int a=len_queue(queue);
+	dequeue(queue);
+	int b=len_queue(queue);
 	return 0;
 }
 
+
+
+//initialisation racine
 bin_tree *initree(int val)
 {
   bin_tree *tree = malloc(sizeof(bin_tree));
@@ -30,14 +59,89 @@ bin_tree *initree(int val)
   return tree;
 }
 
-void setvalue(bin_tree *tree, int elem)
+//ajout fils gauche
+void add_left(int val,bin_tree *tree)
 {
-  tree->value = elem;
+	if(tree->left!=NULL)
+		return;
+	tree->left=initree(val);
+}
+//ajout fils droit
+void add_right(int val,bin_tree *tree)
+{
+	if(tree->right!=NULL)
+		return;
+	tree->right=initree(val);
+}
+//change la valeur du noeud
+void set_value(int val,bin_tree *tree)
+{
+	tree->value=val;
 }
 
-void printleft(bin_tree *tree)
+//parcours pofondeur main gauche en infix
+void printleft_infix(bin_tree *tree)
 {
-	printleft(tree->left);
-	printf("%d/n",tree->value);
-	printleft(tree->left);
+	if (tree==NULL)
+	{	
+		printf("\n");
+		return ;
+	}
+	printleft_infix(tree->left);
+	printf("- %d",tree->value);
+	printleft_infix(tree->right);
 }
+
+
+//file
+Queue *init_queue()
+{
+	Queue *queue = malloc(sizeof(Queue));
+	queue->next=NULL;
+	return queue;
+}
+
+void enqueue(Queue *queue,int val)
+{
+		
+			Queue *temp=init_queue();
+			temp->value=val;
+			if(!is_empty(queue))
+			{
+			temp->next=queue->next;
+			}
+			queue->next=temp;
+}
+
+int dequeue(Queue *queue)
+{
+		Queue *temp=queue;
+		while(temp->next->next!=NULL)
+			temp=temp->next;
+			
+		free(temp->next);
+		temp->next=NULL;
+		return temp->value;
+		
+}
+
+int len_queue(Queue *queue)
+{
+	Queue *temp =queue;
+	int i=0;
+	while(temp->next!=NULL)
+	{
+		temp=temp->next;
+		printf("%d \n",temp->value);
+		i+=1;
+		
+	}
+	return i;
+}
+int is_empty(Queue *queue)
+{
+	return queue->next==NULL;
+}
+
+
+
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 LIB = -lm 
-OPT = -Wall
+OPT = -Wall 
 
 out: Binare_tree.o 
 	gcc Binare_tree.o -o out
  
 Binare_tree.o: Binare_tree.c
-	gcc -c Binare_tree.c -o Binare_tree.o
+	gcc -c -g Binare_tree.c -o Binare_tree.o
 
 all: $(OUTPUT)
 
